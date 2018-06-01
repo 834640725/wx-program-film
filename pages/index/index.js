326df5429eee73b5bcf8a3779ecd829945ac0a63
qiqingfu <723469354@qq.com>
2018-06-01 20:18:40 +0800

    豆瓣接口设置

---
+++ pages/index/index.js
@@ -0,0 +1,29 @@
+// pages/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+     list:[],
+  },
+  onLoad(){
+    this.loadMovies();
+  },
+  loadMovies(){
+    wx.request({
+      url:"https://douban.uieee.com/v2/movie/in_theaters",
+      header:{
+        'Content-Type': 'json',
+      },
+      success:({data}) => {
+        this.setData({
+          list:data
+        })
+        // get data sive in data.list
+        console.log(this.data.list)
+      }
+    })
+  }
+
+})
