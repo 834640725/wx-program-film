623bead2f86309b4b58495e548f5b61abad36c5f
qiqingfu <723469354@qq.com>
2018-06-02 02:09:07 +0800

    电影首屏数据

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,12 +7,14 @@ Page({
   data: {
      list:[],
      start:0,
-     count:8,
+     count:6,
+     loader:false,
   },
   onLoad(){
     this.loadMovies();
   },
   loadMovies(){
+    this.setData({loader:true})
     wx.request({
       url:`https://douban.uieee.com/v2/movie/in_theaters?start=${this.data.start}&count=${this.data.count}`,
       method:"GET",
@@ -23,14 +25,28 @@ Page({
         let arr = [];
         for (let i = 0; i < data.subjects.length; i+=2){
           //将没两个对象放进一个数组中
+          // 上映日期格式转换
+          data.subjects[i].mainland_pubdate = data.subjects[i].mainland_pubdate.split('-').join('/');
+          data.subjects[i+1].mainland_pubdate = data.subjects[i].mainland_pubdate.split('-').join('/');         
           arr.push([data.subjects[i], data.subjects[i+1]])
+
         }
         this.setData({
           list:arr,
+          loader:false,
         })
         console.log(this.data.list)
       }
     })
-  }
+  },
 
+  scrollHandler(){
+    //拖动到底部显示loading,且数据增加两条
+    this.setData({
+      loader:true,
+      count:this.data.count += 4,
+    })
+    console.log(this.data.count)
+     this.loadMovies(); 
+  }
 })
