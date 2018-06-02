620641ef1ce8fe24fd8ccad9521e3eec2083f885
qiqingfu <723469354@qq.com>
2018-06-02 16:02:26 +0800

    subject组件专区,数据跳转

--- pages/movieInfo/movieInfo.js
+++ pages/movieInfo/movieInfo.js
@@ -13,6 +13,9 @@ Page({
    */
   onLoad: function (options) {
      let Id = options.id;
+
+     // 开启导航条加载动画
+     wx.showNavigationBarLoading();
      wx.request({
        url: 'https://douban.uieee.com/v2/movie/subject/' + Id,
        method:"GET",
@@ -23,6 +26,14 @@ Page({
           this.setData({
             moviesData:data,
           })
+          
+          //动态设置电影详情setNavigationBarTitle
+          wx.setNavigationBarTitle({
+            title: this.data.moviesData.title,
+          })
+
+          //数据回来之后隐藏加载动画
+          wx.hideNavigationBarLoading();
           //请求成功同步数据
           console.log(this.data.moviesData)
        })
--- pages/subject/subject.js
+++ pages/subject/subject.js
@@ -5,62 +5,47 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+     type:[
+       {
+         title:"正在热映",
+         ask:"in_theaters",
+         url:"aiqing.png"
+       },
+       {
+         title: "即将上映",
+         ask: "coming_soon",
+         url: "donghua.png"
+       },
+       {
+         title: "Top250",
+         ask: "top250",
+         url: "dongzuo.png"
+       },
+       {
+         title: "口碑榜",
+         ask: "weekly",
+         url: "xiju.png"
+       },
+       {
+         title: "北美票行榜",
+         ask: "us_box",
+         url: "kehuan.png"
+       }
+     ]
+    //  4191645
+    // v2/movie/search?q={text}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  // 跳转专栏页
+  totypeHander(e){
+    let type = e.currentTarget.dataset.title;
+    
   }
 })
